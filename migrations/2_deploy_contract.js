e5988a19fd4f7c35b4f4e9f7441aa1cf06bc330b
Georgios Konstantopoulos <me@gakonst.com>
2018-06-18 15:32:25 +0300

    minor fixes

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,24 +1,22 @@
-const CryptoCards = artifacts.require("CryptoCards");
-const RootChain = artifacts.require("RootChain");
+const Contract = artifacts.require("Contract");
+// const Contract2 = artifacts.require("Contract2");
 
 module.exports = async function(deployer, network, accounts) {
     // return; // for testing
-    let aCryptoCardsInstance;
-    let aRootChainInstance;
+    let aContractInstance;
+    // let aContract2Instance;
 
-    return deployer.deploy(RootChain)
-        .then(() => RootChain.deployed())
+    return deployer.deploy(Contract)
+        .then(() => Contract.deployed())
         .then(instance => {
-            aRootChainInstance = instance;
-            console.log('RootChain deployed at address: ' + instance.address);
-            return deployer.deploy(CryptoCards, instance.address);
-        })
-    .then(() => CryptoCards.deployed())
-        .then((instance) => {
-            aCryptoCardsInstance = instance;
-            console.log('CryptoCards deployed at address: ' + instance.address);
-
-            aRootChainInstance.setCryptoCards(instance.address);
+            aContractInstance = instance;
+            console.log('Contract deployed at address: ' + instance.address);
+            // return deployer.deploy(Contract2, instance.address);
         });
+    // .then(() => Contract2.deployed())
+    //     .then((instance) => {
+    //         aContract2Instance = instance;
+    //         console.log('Contract2Instance deployed at address: ' + instance.address);
+    //     });
 };
 
--- scripts/ganache-cli.sh
+++ scripts/ganache-cli.sh
@@ -3,18 +3,18 @@
 ganache_port=8545
 
 ganache_running() {
-  nc -z localhost "$ganache_port"
+    nc -z localhost "$ganache_port"
 }
 
 start_ganache() {
-  ganache-cli -p $ganache_port -a 15 -i 15 --blocktime 15 --gasLimit 50000000 -e 10000000000000000000000 -m gravity top burden flip student usage spell purchase hundred improve check genre > /dev/null 2>&1 &
-  ganache_pid=$!
-  echo "ganache-cli started with pid $ganache_pid"
+    npx ganache-cli -p $ganache_port -a 15 -i 15 --gasLimit 50000000 -e 100000000000000000000 -m gravity top burden flip student usage spell purchase hundred improve check genre > /dev/null 2>&1 &
+    ganache_pid=$!
+    echo $ganache_pid
 }
 
 if ganache_running; then
-  echo "Using existing ganache instance at port $ganache_port"
+    echo "Using existing ganache instance at port $ganache_port"
 else
-  echo "Starting our own ganache instance at port $ganache_port"
-  start_ganache
+    # echo "Starting our own ganache instance at port $ganache_port"
+    start_ganache
 fi
